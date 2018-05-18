dcfa53231d7a73a669e781f1afeabed00ef26d1c
lzhang06 <lzhang64@stevens.edu>
2018-05-17 21:58:16 -0400

    port num add3

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
 	# port = int(os.environ.get("PORT", 5000))
     # app.run(host='0.0.0.0', port=5000, debug=True)
     # app.run(host='0.0.0.0', port=5000, debug=True)
-    port = int(os.environ.get('PORT', 33507))
+    port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, port=port)
     # app.run(debug=True)
 
