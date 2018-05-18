e75af393e8b7c590abfa246e94db8c099ee65b18
lzhang06 <lzhang64@stevens.edu>
2018-05-17 22:00:44 -0400

    port num add4

--- app.py
+++ app.py
@@ -22,9 +22,7 @@ def read_data():
 
 if __name__ == '__main__':
 	# port = int(os.environ.get("PORT", 5000))
-    # app.run(host='0.0.0.0', port=5000, debug=True)
-    # app.run(host='0.0.0.0', port=5000, debug=True)
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, port=port)
-    # app.run(debug=True)
+    app.run(host='0.0.0.0',  port=port, debug=True)
+
 
