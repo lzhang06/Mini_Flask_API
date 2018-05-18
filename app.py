5b8ab49277bfbe652a287be162fbee0731da5fa7
lzhang06 <lzhang64@stevens.edu>
2018-05-17 21:54:38 -0400

    port num add

--- app.py
+++ app.py
@@ -24,5 +24,7 @@ if __name__ == '__main__':
 	# port = int(os.environ.get("PORT", 5000))
     # app.run(host='0.0.0.0', port=5000, debug=True)
     # app.run(host='0.0.0.0', port=5000, debug=True)
-    app.run(debug=True, use_reloader=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, port=33507)
+    # app.run(debug=True)
 
